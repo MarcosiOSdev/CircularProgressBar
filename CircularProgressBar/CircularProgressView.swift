bc8729f5bfbbaec19bca49462a57cc19ff2ed681
Marcos Felipe <mfelipesp@gmail.com>
2022-07-20 16:17:19 -0300

    Created Progress Bar in format circular for SwiftUI

---
+++ CircularProgressBar/CircularProgressView.swift
@@ -0,0 +1,33 @@
+//
+//  CircularProgressView.swift
+//  CircularProgressBar
+//
+//  Created by marcos.felipe.souza on 20/07/22.
+//
+
+import SwiftUI
+
+struct CircularProgressView: View {    
+    let progress: Double
+    let lineCircularColor: Color = .pink.opacity(0.5)
+    let lineProgressIndicatorColor: Color = .pink
+    let strokeStyle: StrokeStyle = StrokeStyle(lineWidth: 30, lineCap: .round)
+    var middleView: AnyView? = nil
+    
+    var body: some View {
+        ZStack(alignment: .center) {
+            Circle()
+                .stroke(lineCircularColor,lineWidth: 30)
+            
+            Circle()
+                .trim(from: 0, to: progress)
+                .stroke(
+                    lineProgressIndicatorColor,
+                    style: strokeStyle)
+                .rotationEffect(.degrees(-90))
+                .animation(.easeOut, value: progress)
+            
+            middleView
+        }
+    }
+}
--- CircularProgressBar/ContentView.swift
+++ CircularProgressBar/ContentView.swift
@@ -4,13 +4,44 @@
 //
 //  Created by marcos.felipe.souza on 20/07/22.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State
+    private var progress = 0.0
+    
+    var middleView: some View {
+        VStack {
+            Image("Lupa")
+                .resizable()
+                .frame(width: 18, height: 18)
+                .scaledToFit()
+            Text("\(progress * 100, specifier: "%.0f")").bold()
+                .font(.largeTitle)
+        }
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            
+                CircularProgressView(
+                    progress: progress,
+                    middleView: AnyView(middleView)
+                    )
+                    .frame(width: 200, height: 200)
+                
+            Slider(value: $progress, in: 0...1)
+                .padding(.all, 32)
+            Button("Reset") {
+                resetProgress()
+            }.buttonStyle(.borderedProminent)
+        }
+    }
+    
+    func resetProgress() {
+        progress = 0
     }
 }
 
