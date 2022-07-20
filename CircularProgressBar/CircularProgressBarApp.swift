252326d5e1cc4fe8a22b875cc21c86d83bbddc08
Marcos Felipe <mfelipesp@gmail.com>
2022-07-20 15:40:59 -0300

    Initial Commit

---
+++ CircularProgressBar/CircularProgressBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularProgressBarApp.swift
+//  CircularProgressBar
+//
+//  Created by marcos.felipe.souza on 20/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CircularProgressBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
